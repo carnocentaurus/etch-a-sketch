110f38c286cc35465f648bd8794ecfe32fe78bfc
carnocentaurus <carnocentaurus@gmail.com>
2026-08-04 09:28:18 +0800

    Initial box generation logic using for loop

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 15; i++) {
+    const box = document.createElement('div');
+    box.style.width = '100px';
+    box.style.height = '100px';
+    box.style.backgroundColor = '#333';
+    document.body.appendChild(box);
+}
