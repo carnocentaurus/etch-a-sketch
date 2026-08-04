b8f96fa3951dad67b40bbf98401176340543a6ce
carnocentaurus <carnocentaurus@gmail.com>
2026-08-04 09:41:15 +0800

    Smaller boxes and initial #grid-container styles to add visual seperation between boxes

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const gridContainer = document.querySelector('#grid-container');
 for (let i = 0; i <= 15; i++) {
     const box = document.createElement('div');
 
-    box.style.width = '100px';
-    box.style.height = '100px';
+    box.style.width = '20px';
+    box.style.height = '20px';
     box.style.backgroundColor = '#333';
 
     gridContainer.appendChild(box);
