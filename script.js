e78ad3a446a2994b8727f1e9f669ade092c063e5
carnocentaurus <carnocentaurus@gmail.com>
2026-08-04 09:47:01 +0800

    Nested for loop to generate 16 boxes 16 times and flex-wrap

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gridContainer = document.querySelector('#grid-container');
 
+
 for (let i = 0; i <= 15; i++) {
-    const box = document.createElement('div');
+    for (let i = 0; i <= 15; i++) {
+        const box = document.createElement('div');
 
-    box.style.width = '20px';
-    box.style.height = '20px';
-    box.style.backgroundColor = '#333';
+        box.style.width = '20px';
+        box.style.height = '20px';
+        box.style.backgroundColor = '#333';
 
-    gridContainer.appendChild(box);
+        gridContainer.appendChild(box);
+    }
 }
