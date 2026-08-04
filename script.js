8739beea999a189761012bdbc4c5d59f32c9ce7a
carnocentaurus <carnocentaurus@gmail.com>
2026-08-04 11:39:27 +0800

    Refactor box generation to create columns and append boxes correctly

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const gridContainer = document.querySelector('#grid-container');
 
 
 for (let i = 0; i <= 15; i++) {
+    const column = document.createElement('div');
+    column.style.display = 'flex';
+    column.style.flexDirection = 'column';
+    column.style.gap = '0.2rem';
+
     for (let i = 0; i <= 15; i++) {
         const box = document.createElement('div');
 
@@ -9,6 +14,7 @@ for (let i = 0; i <= 15; i++) {
         box.style.height = '20px';
         box.style.backgroundColor = '#333';
 
-        gridContainer.appendChild(box);
+        column.appendChild(box);
     }
+    gridContainer.appendChild(column);
 }
