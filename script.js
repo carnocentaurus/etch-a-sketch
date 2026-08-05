bf45d3096027164031bf2194a2b6ab098c50117d
carnocentaurus <carnocentaurus@gmail.com>
2026-08-05 09:16:17 +0800

    Increase grid container size to 600px and used a combination flex properties and dynamic width/height values for better grid scaling

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const gridContainer = document.querySelector('#grid-container');
 
+const boxes = 100;
 
-for (let i = 0; i <= 15; i++) {
+
+for (let i = 1; i <= boxes; i++) {
     const column = document.createElement('div');
     column.style.display = 'flex';
     column.style.flexDirection = 'column';
-    column.style.gap = '0.2rem';
+    column.style.flexGrow = '1';
+    column.style.gap = '1px';
 
-    for (let i = 0; i <= 15; i++) {
+    for (let i = 1; i <= boxes; i++) {
         const box = document.createElement('section');
 
-        box.style.width = '20px';
-        box.style.height = '20px';
+        box.style.display = 'flex';
+        box.style.flexGrow = '1;'
+        box.style.width = 'auto';
+        box.style.height = 'calc(600px / 20)';
         box.style.backgroundColor = '#353839';
 
         column.appendChild(box);
